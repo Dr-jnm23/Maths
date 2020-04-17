9838ee55ec781e8f1e826e415738039df2feecbc
James Martin <63636440+Dr-jnm23@users.noreply.github.com>
2020-04-17 13:20:14 +0100

    Add files via upload

---
+++ Lesson1,variables.py
@@ -0,0 +1,64 @@
+# This is a summary of the 7 data types in Python
+# useful tip for these notes: comment a block using
+# ctrl {with} / {with} +
+
+# Text Type:
+# str
+
+# Numeric Types:
+# int, float, complex
+
+# Sequence Types:
+# list, tuple, range
+
+# Mapping Type:
+# dict
+
+# Set Types:
+# set, frozenset
+
+# Boolean Type:
+# bool
+
+# Binary Types:
+# bytes, bytearray, memoryview
+
+# store and print a NUMERIC variable #
+x=7
+print(x)
+
+# store and print a STRING variable
+y = 'she said "hello"' 
+print(y)
+
+# store and print a BOOLEAN variable
+# N.B. = not quotes, capital
+z = True
+print(z)
+
+#store and print a FLOAT variable
+p = 1.2
+print(p)
+
+#store and print a tuple (not mutable)
+cars = ('911','AMV8','355')
+print(cars)
+
+#store and print a list (mutable)
+grand_slam = ['Aus','Fre','Wim']
+
+#store and print a set
+set = {'1','apple','2','pear'}
+
+# return types of variable from above
+type(x)
+type(y)
+type(z)
+type(p)
+type(cars)
+type(grand_Slam)
+
+# multiple assignment of variables
+
+a, b, c = 1, 2, 3
+team1, team2, team3 = 'Man City','Man U','CFC'
